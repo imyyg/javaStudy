50c30b80a30bbb010cfbeb7046371da543d17d49
imyyg <1309658763@qq.com>
2021-06-25 14:13:57 +0800

    uodate

--- day0624/VariableTest.java
+++ day0624/VariableTest.java
@@ -1,14 +1,18 @@
 class VariableTest{
 	public static void main(String[] args){
+
+        //编译通过，输出123
+        //表明long型变量的值后不加L不会报错，会被当作int型
 		long l1 = 123;
-        int i1 = l1 + 5;
-        System.out.println(i1); //编译通过，输出128
+        System.out.println(l1);
         
         //编译不通过，21883843298798432超出int型最大范围
-        // long l2 = 21883843298798432;
-        // int i2 = l2;
+        long l2 = 21883843298798432;
+        int i2 = l2;
         
-        //编译不通过，12.8被默认为double型，不能将double型常量赋值给float型变量
+        //编译不通过:错误: 不兼容的类型: 从double转换到float可能会有损失
+        //12.8被默认为double型，不能将double型常量赋值给float型变量
         float f1 = 12.8;
+        System.out.println(f1);
 	}
 }
